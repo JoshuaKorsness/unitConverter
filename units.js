7b1f68ee675546b75a856548081cb460683e13a1
Josh Korsness <joshkorsness@gmail.com>
2019-03-16 08:20:40 -0700

    Unit Converter v1

---
+++ units.js
@@ -0,0 +1,58 @@
+const unitScalars = [
+	{
+		"name": "milligrams",
+		"abr": "mg",
+		"type": "mass",
+		"anchor": "grams",
+		"scalar": 1/1000
+	},
+	{
+		"name": "grams",
+		"abr": "g",
+		"type": "mass",
+		"anchor": "grams",
+		"scalar": 1
+	},
+	{
+		"name": "kilograms",
+		"abr": "kg",
+		"type": "mass",
+		"anchor": "grams",
+		"scalar": 1000
+	},
+	{
+		"name": "pounds",
+		"abr": "lbs",
+		"type": "weight",
+		"anchor": "newtons",
+		"scalar": 4.44822
+	},
+	{
+		"name": "newtons",
+		"abr": "N",
+		"type": "weight",
+		"anchor": "newtons",
+		"scalar": 1
+	},
+	{
+		"name": "newton-meters",
+		"abr": "Nm",
+		"type": "torque",
+		"anchor": "newton-meters",
+		"scalar": 1
+	},
+	{
+		"name": "inch-pounds",
+		"abr": "in-lbs",
+		"type": "torque",
+		"anchor": "newton-meters",
+		"scalar": 0.112984829
+	},
+	{
+		"name": "foot-pounds",
+		"abr": "ft-lbs",
+		"type": "torque",
+		"anchor": "newton-meters",
+		"scalar": 1.3558
+	},
+];
